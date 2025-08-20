e776b674fd98c293cccd97c53935ccfe63b53eee
Qainguin <slimaster14@gmail.com>
2025-08-20 13:21:39 -0500

    Finished Theming (for now)

--- src/lib/components/Search.svelte
+++ src/lib/components/Search.svelte
@@ -9,7 +9,7 @@
 </script>
 
 <input
-	class="text-primary-accent hover:border-secondary-accent h-fit rounded-lg border border-primary-accent bg-transparent px-2 py-1 not-sm:w-full not-sm:text-center focus:border-secondary-accent focus:ring-0 focus:outline-none"
+	class="text-primary-accent hover:border-hover-accent border-primary-accent focus:border-focus-accent h-fit rounded-lg border bg-transparent px-2 py-1 not-sm:w-full not-sm:text-center focus:ring-0 focus:outline-none"
 	placeholder="Search"
 	bind:value={query}
 />
